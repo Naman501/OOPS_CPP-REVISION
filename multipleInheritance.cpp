0bb882756894842a2c4b256448c74f93e2fdfecd
Naman Mittal <namanmittal501@gmail.com>
2025-06-05 15:04:03 +0530

    Multiple Inheritance

---
+++ multipleInheritance.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Cricketer{
+    public:
+    int reuns;
+    int wickets;
+    int avg;
+};
+class Engineer{
+    public:
+    int experience;
+    string domain;
+    
+};
+
+class Phodu : public Engineer,Cricketer{
+public:
+};
+
+int main(){
+
+
+    return 0;
+}
